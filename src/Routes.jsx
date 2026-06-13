75f42256bbe807a338983630fa47e73fe54719f1
joao <joaovitorsantossouza937@gmail.com>
2026-06-13 18:38:39 -0300

    feat:configure the routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/home"
+import Detalhes from "./pages/detalhes"
+import Colecao from "./pages/colecao"
 
 const RoutApp = () => {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={ <Home/> }/>
+                <Route path="/detalhes/:id" element={<Detalhes/>}/>
+                <Route path="/colecao" element={ <Colecao/>}/>
+                
             </Routes>
         </BrowserRouter>
     )
