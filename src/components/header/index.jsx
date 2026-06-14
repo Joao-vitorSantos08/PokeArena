415f14f5fff3adb0aa4d117901e562d27ae2c2c9
joao <joaovitorsantossouza937@gmail.com>
2026-06-14 12:02:54 -0300

    feat: Creates the header component.

---
+++ src/components/header/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import "./header.css"
+
+const Header = () => {
+    return(
+        <header>
+            <ul>
+                <Link to={"/"} className="logo">PokaArena</Link>
+                <Link>Sala de Batalha</Link>
+                <Link to={"/colecao"} className="colecao">Minha Coleção</Link>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
